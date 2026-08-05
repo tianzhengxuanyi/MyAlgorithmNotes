b51ff8c37680068048f1bbf517325ece0976aafe
谢飞 <1145986889@qq.com>
2026-08-05 22:37:45 +0800

    feat: code

---
+++ interview/code/throttle.js
@@ -0,0 +1,23 @@
+function throttle(fn, delay) => {
+  let timer = null;
+  return function (...args) {
+    if (timer) {
+      return;
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+      timer = null;
+    }, delay);
+  }
+}
+
+function throttle(fn, delay) => {
+  let lastTime = 0;
+  return function (...args) {
+    const now = Date.now();
+    if (now - lastTime >= delay) {
+      lastTime = now;
+      fn.apply(this, args);
+    }
+  }
+}
